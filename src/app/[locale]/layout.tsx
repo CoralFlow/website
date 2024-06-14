d7cd096ac69a9f72f90fe2a5829b866cc36070bc
Sunil <linusaltacc01@protonmail.com>
2024-06-14 22:16:43 +0530

    update favicon

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -9,16 +9,47 @@ import { ThemeProvider } from "@/components/theme-provider"
 export const metadata: Metadata = {
   icons: [
     {
+      rel: 'apple-touch-icon',
+      url: '/apple-touch-icon.png',
+    },
+    {
       rel: 'icon',
       type: 'image/png',
       sizes: '32x32',
-      url: '/favicon.svg',
+      url: '/favicon-32x32.png',
     },
     {
       rel: 'icon',
       type: 'image/png',
       sizes: '16x16',
-      url: '/favicon.png',
+      url: '/favicon-16x16.png',
+    },
+    {
+      rel: 'icon',
+      url: '/favicon.ico',
+    },
+    {
+      rel: 'manifest',
+      url: '/site.webmanifest',
+    },
+    {
+      rel: 'android-chrome',
+      sizes: '192x192',
+      url: '/android-chrome-192x192.png',
+    },
+    {
+      rel: 'android-chrome',
+      sizes: '512x512',
+      url: '/android-chrome-512x512.png',
+    },
+    {
+      rel: 'mask-icon',
+      color: '#000000',
+      url: '/safari-pinned-tab.svg',
+    },
+    {
+      rel: 'shortcut icon',
+      url: '/favicon.ico',
     },
   ],
 };
